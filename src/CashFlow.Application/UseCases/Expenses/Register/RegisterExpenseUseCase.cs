d9e88a61bc170d26aef7a24c44f4e1cc3f6cef0a
Bruno <brunopan08@gmail.com>
2025-05-19 21:20:27 -0300

    Add use cases

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,12 +1,17 @@
 ﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register
 {
     public class RegisterExpenseUseCase
     {
-        public void Execute(RequestExpenseJson request)
+        public ResponseRegisterExpenseJson Execute(RequestExpenseJson request)
         {
+            var useCase = new RegisterExpenseUseCase();
 
+            var response = useCase.Execute(request);
+
+            return new ResponseRegisterExpenseJson();
         }
     }
 }
--- src/CashFlow.Communication/obj/Debug/net8.0/CashFlow.Communication.AssemblyInfo.cs
+++ src/CashFlow.Communication/obj/Debug/net8.0/CashFlow.Communication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Communication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35508f88ea9c12c952a1933724cb389378c6063")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Communication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Communication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
